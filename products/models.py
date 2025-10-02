dadeb5fdabfee846c220ab0681baec7ad47ab32e
alireza200411 <alirezamoradi292048@gmail.com>
2025-10-02 19:55:52 +0330

    fix: Removing related_name from the parent field in the Category model

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True, blank=True, null=True)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='parent')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to='category/', null=True, blank=True)
 
