1781e9ed2388aaba5aebe66f3a5de6605b9b8a8f
alireza200411 <alirezamoradi292048@gmail.com>
2025-10-03 20:33:05 +0330

    feat: add parent field with validation to serializer

--- products/serializers.py
+++ products/serializers.py
@@ -3,7 +3,15 @@ from .models import Category
 
 
 class CategorySerializers(serializers.ModelSerializer):
+    parent = serializers.SlugRelatedField(slug_field='name', queryset=Category.objects.all(), required=False, allow_null=True)  #to read and write the parent field by object name instead of ID
+
     class Meta:
         model = Category
-        fields = ('name', 'image', 'description', 'id')
+        fields = ('name', 'image', 'description', 'id', 'parent')
         read_only_fields = ['id', ]
+
+    def validate_parent(self, value):
+        if Category.objects.filter(name=value).exists():
+            return value
+        else:
+            raise serializers.ValidationError("There is no category!")
