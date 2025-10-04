0daeb95b954662e10c159e2b6dd33ad03c3c3ac8
alireza200411 <alirezamoradi292048@gmail.com>
2025-10-04 22:08:42 +0330

    add: product model

--- products/models.py
+++ products/models.py
@@ -15,3 +15,23 @@ class Category(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         return super().save()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, min_value=0)
+    discount_price = models.DecimalField(null=True)
+    stock = models.PositiveIntegerField(default=0)
+    image = models.ImageField(pload_to='product/', null=True, blank=True)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save()
