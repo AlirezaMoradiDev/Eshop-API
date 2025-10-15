7dbbb880420a0b0241852ab598e2ddfdbcfc446f
alireza200411 <alirezamoradi292048@gmail.com>
2025-10-15 15:41:10 +0330

    add: product admin

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "parent", )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'description', 'created_at', )
 
