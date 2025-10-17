461c626eaab7e47f222ea445b54c03549cbd7804
alireza200411 <alirezamoradi292048@gmail.com>
2025-10-17 13:52:00 +0330

    add: serializer product

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Product
 
 
 class CategorySerializers(serializers.ModelSerializer):
@@ -15,3 +15,27 @@ class CategorySerializers(serializers.ModelSerializer):
             return value
         else:
             raise serializers.ValidationError("There is no category!")
+
+
+class ProductSerializers(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='name', queryset=Category.objects.all(), required=False, allow_null=True)
+
+    class Meta:
+        model = Product
+        fields = ('id' ,'name', 'description', 'price', 'discount_price', 'stock', 'created_at', 'updated_at', 'category')
+        read_only_fields = ['id', 'created_at', 'updated_at', ]
+
+    def validate(self, attrs):
+        name = attrs.get('name')
+        category = attrs.get('category')
+        if attrs.get('discount_price'):
+            discount = attrs.get('discount_price')
+            if discount > attrs.get('price'):
+                raise serializers.ValidationError('The discount must be less than the product price.')
+        if category.product_set.filter(name=name).exists(): # or Product.objects.filter(name=name, category=category)
+            raise serializers.ValidationError('name is exist')
+
+        return attrs
+
+
+
