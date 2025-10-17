6486fa3eeb8451f1d6e11761afcc69500399d73a
alireza200411 <alirezamoradi292048@gmail.com>
2025-10-17 13:48:44 +0330

    fix: update_at, category field in product model

--- products/models.py
+++ products/models.py
@@ -27,8 +27,8 @@ class Product(models.Model):
     stock = models.PositiveIntegerField(default=0)
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
