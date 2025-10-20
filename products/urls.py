a1ee5d783c88ca2a5b6a579896223707a47bc354
alireza200411 <alirezamoradi292048@gmail.com>
2025-10-20 11:04:06 +0330

    feat: Product CRUD

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 app_name = 'products'
 urlpatterns = [
-    path('add', views.add_category, name='add'),
+    path('add/category', views.add_category, name='add_category'),
     path('read', views.read_all_category, name='read'),
+    path('products', views.ProductAPI.as_view(), name='product_read_write'),
+    path('products/<int:pk>', views.ProductAPI.as_view(), name='product_edit'),
     path('update/<int:pk>', views.update_obj_category, name='update'),
     path('delete/<int:pk>', views.delete_category, name='delete')
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAdminUser
-from .serializers import CategorySerializers
-from .models import Category
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.views import APIView
+from .serializers import CategorySerializers, ProductSerializers
+from .models import Category, Product
 
 @api_view(['POST'])
 @permission_classes([IsAdminUser]) #  is_authenticated=True  ---->  is_staff=True ---->  ...
@@ -56,3 +57,64 @@ def delete_category(request, pk):
         return Response({"error": "Category not found"})
     obj_catg.delete()
     return Response({"response": "deleted"})
+
+
+class ProductAPI(APIView):
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PATCH', 'DELETE']:
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
+    def get(self, request):
+        if request.method == "GET":
+            products = Product.objects.all()
+            if request.GET.get('category') or request.GET.get('price'):
+                if not request.GET.get('category'):
+                        products = Product.objects.filter(price=request.GET.get('price'))
+                elif not request.GET.get('price'):
+                    products = Product.objects.filter(category__name=request.GET.get('category'))
+                else:
+                    products = Product.objects.filter(category__name=request.GET.get('category'), price=request.GET.get('price'))
+            serializer = ProductSerializers(instance=products, many=True)
+            return Response(serializer.data)
+
+    @permission_classes([IsAdminUser])
+    def post(self, request):
+        if request.method == "POST":
+            serializer = ProductSerializers(data=request.data)
+            if serializer.is_valid():
+                print(request.data.get('category'))
+                serializer.save()
+                return Response({'response': 'added'})
+            else:
+                return Response(serializer.errors)
+
+    @permission_classes([IsAdminUser])
+    def patch(self, request, pk):
+        if request.method == "PATCH":
+            try:
+                product_obj = Product.objects.get(id=pk)
+            except Product.DoesNotExist:
+                return Response({'error': "Product not found"})
+            serializer = ProductSerializers(instance=product_obj, data=request.data, partial=True)
+            if request.data.get('category') is not None:
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response({'response': f'{product_obj.name} updated'})
+                else:
+                    return Response(serializer.errors)
+            else:
+                return Response({'error': 'You must enter a category.'})
+
+    @permission_classes([IsAdminUser])
+    def delete(self, request, pk):
+        if request.method == "DELETE":
+            try:
+                product_obj = Product.objects.get(id=pk)
+            except Product.DoesNotExist:
+                return Response({'error': "Product not found"})
+
+            product_obj.delete()
+            return Response({'response': f'{product_obj.name} deleted'})
